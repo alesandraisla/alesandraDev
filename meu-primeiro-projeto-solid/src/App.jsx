e7ece31d423d419a3991b03273e54cdcd4edb0a9
AlesandraIsla <alesandramempis@gmail.com>
2023-07-16 21:30:35 -0300

    Primeiro projeto usando solidjs

---
+++ meu-primeiro-projeto-solid/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <h2 class={styles.Title}>Hello World!!!</h2>
+    </div>
+  );
+}
+
+export default App;
