799f6f4aa2fd64f26dfa7f30c02848d3cfe83fdb
AlesandraIsla <alesandramempis@gmail.com>
2023-08-06 21:43:08 -0300

    componente accordion com solidjs

--- meu-primeiro-projeto-solid/src/App.jsx
+++ meu-primeiro-projeto-solid/src/App.jsx
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
+import { Accordion } from './components/Accordion';
 
 function App() {
+  const items = [
+    {title: "Item 1", content: "Content for item 1"},
+    {title: "Item 2", content: "Content for item 2"},
+    {title: "Item 3", content: "Content for item 3"},
+    {title: "Item 4", content: "Content for item 4"},
+    {title: "Item 5", content: "Content for item 5"},
+  ]
+
   return (
     <div class={styles.App}>
-      <h2 class={styles.Title}>Hello World!!!</h2>
+      <h2 class={styles.Title}>Criando Accordion com SolidJs</h2>
+      <Accordion items={(items)} />
     </div>
   );
 }
---
+++ meu-primeiro-projeto-solid/src/components/Accordion.jsx
@@ -0,0 +1,38 @@
+import { VsArrowDown } from 'solid-icons/vs'
+import { RiArrowsArrowRightSLine } from 'solid-icons/ri'
+import { createSignal } from 'solid-js';
+import './accordionStyle.css'
+
+function AccordionItem({title, content}) {
+  const [isOpen, setIsOpen] = createSignal(false)
+  return(
+    <div class="acc-item">
+      <div class="acc-header" onClick={() => setIsOpen(!isOpen())}>
+        {title}
+        <span>
+          {isOpen() ?
+            <VsArrowDown/>
+            :
+            <RiArrowsArrowRightSLine/>
+        }
+        </span>
+      </div>
+      {isOpen() &&(
+        <div class="acc-content">
+            {content}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function Accordion({items}) {
+  return(
+    <div class="accordion">
+      {items.map((item, index) => (
+        <AccordionItem key={index} title={item.title} content={item.content} />
+      ))}
+    </div>
+  )
+}
+
