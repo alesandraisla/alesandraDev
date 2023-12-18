3eb274f1fe6d35dbaac39d1f790f94cae851c027
AlesandraIsla <alesandramempis@gmail.com>
2023-12-17 22:22:32 -0300

    Arvore de natal

---
+++ especial-natal/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const snowflakesContainer = document.getElementById("snowflakes")
+
+  for(let i = 0; i < 50; i++) {
+    createSnowflake(snowflakesContainer)
+  }
+})
+
+function createSnowflake(container) {
+  const snowflake = document.createElement("div");
+  snowflake.className = "snowflake";
+  snowflake.style.left = `${Math.random() * 100}vw`;
+  snowflake.style.animationDuration = `${Math.random() * 4 + 4}s`
+  snowflake.style.animationDelay = `${Math.random()}s`
+  container.appendChild(snowflake);
+}
